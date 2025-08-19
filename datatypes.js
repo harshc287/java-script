8035d8da8dbb350fff97fd915304f3259337ce9a
harshc287 <chouhanharsh332@gmail.com>
2025-08-19 23:02:25 +0530

    completed

---
+++ datatypes.js
@@ -0,0 +1,66 @@
+x=10                                            //number data type
+y=5
+console.log("addition of x and y is ", x+y)
+console.log("difference", x-y)
+
+
+
+favoriteMovie = 'TheBoys'                       //string
+message = ' I love to watch ' + favoriteMovie
+
+console.log(message)
+
+
+isHungry= true
+if(isHungry){                                      //boolien data type
+    console.log("healthy khana; khana he")       //this print when true
+}else{
+    console.log("contunue your work")
+}
+
+
+user = {name: 'Harsh', age:20}                  //object dt
+console.log(user.name , user.age)
+
+
+fruit= [ 'Apple' , 'orange', 'mango' ]          //array
+console.log(fruit[1])
+
+
+let mystry                                      //undefined
+console.log(mystry)
+
+mystry = null                                   //null
+console.log(mystry)
+
+
+symbolOne= Symbol('harsh')                      //symbol
+
+symbolTwo = Symbol('harsh')
+
+one = 'harsh'
+two = 'harsh'
+if(symbolOne==symbolTwo){
+    console.log('both are same')
+}else{
+    console.log("thet are unique")
+}
+
+
+if(one==two){
+    console.log('both are same')
+}else{
+    console.log("thet are unique")
+}
+
+p= 555
+q = '555'
+
+if (p==q){
+    console.log("botn p and q are same ")
+}else{
+    console.log("p and q are different")
+}
+
+
+
